21fae77f315b223e892368124d78f4b696fe4545
ThrockenOy <throckenoy@mail.ru>
2025-09-17 17:28:07 +0300

    Скрипт для автосвета

---
+++ get-led/auto-light.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+led = 26 
+GPIO.setup(led, GPIO.OUT)
+photo = 6
+GPIO.setup(photo, GPIO.IN)
+
+state = 0
+
+
+
+while True:
+    state = GPIO.input(photo)
+    GPIO.output (led, 1 - state)
+    time.sleep(0.1)
