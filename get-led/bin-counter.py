e6d19602fa8bcf0026341498435a82d862e19b35
ThrockenOy <throckenoy@mail.ru>
2025-09-24 17:03:05 +0300

    Изменен файл с двоичным счетчиком

--- get-led/bin-counter.py
+++ get-led/bin-counter.py
@@ -1,16 +1,41 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.cleanup()
+
 GPIO.setmode(GPIO.BCM)
 
-leds = [16, 12, 25, 17, 23, 22, 24]
-
+up = 9
+down = 10
+leds = [24, 22, 23, 27, 17, 25, 12, 16]
+GPIO.setup (leds, GPIO.OUT)
+GPIO.setup (up, GPIO.IN)
+GPIO.setup (down, GPIO.IN)
 num = 0
+
+
 
 def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
-sleep_time = 0.2
-if GPIO.input(up):
-    num =num+1
-    print(num, dec2bin(num))
-    time.sleep(sleep_time)    
+sleep_time = 0,2
+while True:
+    num = int(input())
+
+    if GPIO.input(up):
+        if -1<num<254:
+            num =num+1
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
+        else:
+            print('0')    
+
+    if GPIO.input(down):
+        if 0<num<255:
+            num =num-1
+            print(num, dec2bin(num))
+            GPIO.output()
+            time.sleep(sleep_time)  
+        else:
+            print('0')
+    for i, pin in leds:
+        GPIO.OUT(pin, int(dec2bin(num)[i]))
