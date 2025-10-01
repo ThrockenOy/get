a07ef159f5ebe54ce63db81b1b9dd2cc0675796e
ThrockenOy <throckenoy@mail.ru>
2025-10-01 18:23:42 +0300

    New

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -1,27 +1,21 @@
 import RPi.GPIO as GPIO
-import time
 
 GPIO.setmode(GPIO.BCM)
 
-pins = [22, 27, 17, 26, 25, 21, 20, 16]
+pins = [16, 20, 21, 25, 26, 17, 27, 22]
 GPIO.setup(pins, GPIO.OUT)
 
 dynamic_range = 3.3
 
-
 def voltage_to_number(voltage):
     if not (0.0 <= voltage <= dynamic_range):
-        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В")
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В)")
         print("Устанавливаем 0.0 В")
         return 0
-
     return int(voltage/dynamic_range*255)
 
 def number_to_dac(number):
     bits = [int(element) for element in bin(number)[2:].zfill(8)]
-    for i in range (len(pins)):
-        GPIO.output(pins[i], number_to_dac(number)[i])
-    print(number, number_to_dac(number))
     return bits
 
 
@@ -31,7 +25,10 @@ try:
         try:
             voltage = float(input("Введите напряжение в Вольтах: "))
             number = voltage_to_number(voltage)
-            number_to_dac(number)
+            bits = number_to_dac(number)
+            for i in range(len(pins)):
+                GPIO.output(pins[i], bits[i])
+            print("Число на вход ЦАП", number, "биты", bits)
 
         except ValueError:
             print("Вы ввели не число. Попробуйте ещё раз\n")
@@ -40,5 +37,3 @@ try:
 finally:
     GPIO.output(pins, 0)
     GPIO.cleanup()
-
-
